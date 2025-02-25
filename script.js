1f9fb4d48e41604a89475dc4b10cbe891180e49e
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-25 17:16:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// # EXERCISE ex-js-async-chef
+
+async function fetchDataJson(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+async function getChefBirthday(id) {
+  let recipe;
+  let user;
+
+  try {
+    recipe = await fetchDataJson(`https://dummyjson.com/recipes/${id}`);
+    // console.log("recipe: ", recipe);
+  } catch {
+    throw new Error("Non è stato possibile trovare la ricetta richiesta");
+  }
+
+  if (recipe.message) {
+    throw new Error(recipe.message);
+  }
+
+  try {
+    user = await fetchDataJson(`https://dummyjson.com/users/${recipe.userId}`);
+    // console.log("User: ", user);
+    // console.log("Data di nascita user: ", user.birthDate);
+  } catch {
+    throw new Error(
+      "Non è stato possibile trovare l'utente legato alla ricetta richiesta"
+    );
+  }
+
+  if (user.message) {
+    throw new Error(user.message);
+  }
+
+  return dayjs(user.birthDate).format("DD/MM/YYYY");
+}
+
+getChefBirthday(1)
+  .then((birthday) => console.log("Data di nascita dello chef:", birthday))
+  .catch((error) => console.error("Errore:", error.message));
